0087a82ea86775c558729675915d8ffb69bc30da
Ainis <ainis@seesense.cc>
2020-08-15 03:36:45 +0300

    Stubbed out SignIn UI

--- PastebinAndroid/app/src/main/java/com/askominas/pastebinandroid/viewmodels/SignInViewModel.kt
+++ PastebinAndroid/app/src/main/java/com/askominas/pastebinandroid/viewmodels/SignInViewModel.kt
@@ -1,5 +1,13 @@
 package com.askominas.pastebinandroid.viewmodels
 
+import androidx.lifecycle.MutableLiveData
 import com.askominas.pastebinandroid.core.BaseViewModel
 
-class SignInViewModel : BaseViewModel()
+class SignInViewModel : BaseViewModel() {
+    val username = MutableLiveData<String>()
+    val password = MutableLiveData<String>()
+
+    fun onSignInClick() {
+        // Test
+    }
+}
